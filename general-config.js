27883268ef2d9eda0b450536c681b05da393eed7
Daniel Polo Campo <84636480+dpolo-dev@users.noreply.github.com>
2024-10-03 15:26:20 -0500

    add s

--- general-config.js
+++ general-config.js
@@ -1,3 +1,3 @@
-export const apiUrl = 'http://tp-poc-transcription-back-db-bghta2h7hcgfabbb.westus-01.azurewebsites.net'
+export const apiUrl = 'https://tp-poc-transcription-back-db-bghta2h7hcgfabbb.westus-01.azurewebsites.net'
 export const regionSpeech = 'eastus'
 export const subscriptionKeySpeech = 'b17af7176cdc4c5994584beda1d50905'
