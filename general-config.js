9e3de182f62ecb579738a6ca905602b3cd46f495
Daniel Polo Campo <84636480+dpolo-dev@users.noreply.github.com>
2024-11-07 12:19:01 -0500

    update back

--- general-config.js
+++ general-config.js
@@ -1,5 +1,5 @@
-// export const apiUrl = 'https://tp-poc-transcription-back-db-bghta2h7hcgfabbb.westus-01.azurewebsites.net'
-export const apiUrl = "http://localhost:5000";
+export const apiUrl = 'https://tp-poc-transcription-back-db-bghta2h7hcgfabbb.westus-01.azurewebsites.net'
+// export const apiUrl = "http://localhost:5000";
 
 export const regionSpeech = "eastus";
 export const subscriptionKeySpeech = "b17af7176cdc4c5994584beda1d50905";
